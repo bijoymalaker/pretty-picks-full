32cb8e71d4f0af2ef9781aaac2dba057a884ec62
bijoymalaker <bijoymalaker7@gmail.com>
2025-09-01 16:40:43 +0600

    SSL integration completed

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\SslCommerzPaymentController;
 
 Route::get('/', function () {
     return Inertia::render('Home');
@@ -125,6 +126,22 @@ Route::prefix('admin')->name('admin.')->group(function () {
         })->name('blogs');
     });
 });
+
+
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
